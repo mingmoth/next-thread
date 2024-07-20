ff4441f1a1b86060c863e247e487b6a541c03179
mingmoth <mingmoth@gmail.com>
2024-07-20 14:50:07 +0800

    feat: move ClerkProvider to root layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,7 @@
-import { ClerkProvider } from "@clerk/nextjs"
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <ClerkProvider>
-      <main>
-        <h1>Threads</h1>
-        <UserButton />
-      </main>
-    </ClerkProvider>
+    <main>
+      <h1>Threads</h1>
+    </main>
   );
 }
