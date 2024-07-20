d4ff6fa1cc0f20bd5da397f6a004310aa04612f8
mingmoth <mingmoth@gmail.com>
2024-07-20 14:43:17 +0800

    feat: add ClerkProvider & UserButton in root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Threads</h1>
-    </main>
+    <ClerkProvider>
+      <main>
+        <h1>Threads</h1>
+        <UserButton />
+      </main>
+    </ClerkProvider>
   );
 }
