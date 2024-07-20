2ef9e555877a84f6326b8385ad4fc6cdfb1e810e
mingmoth <mingmoth@gmail.com>
2024-07-20 16:34:01 +0800

    feat: add SignedIn & SignOutButton in LeftSidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -2,13 +2,13 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
 // constants
 import { sidebarLinks } from "@/constants";
 
 export default function LeftSidebar() {
   const pathname = usePathname();
-  const router = useRouter();
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
@@ -25,6 +25,16 @@ export default function LeftSidebar() {
             </Link>
           )
         })}
+      </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton redirectUrl="/sign-in">
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image src={"/assets/logout.svg"} alt="logout" width={24} height={24} />
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
   );
