c8268e158ac531403b2d520ee9f2f99b881081fe
mingmoth <mingmoth@gmail.com>
2024-07-20 16:19:11 +0800

    feat: add constant index.ts for sidebarLinks & use it in LeftSidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,7 +1,24 @@
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function LeftSidebar() {
-    return (
-        <div className="left-sidebar">
-            LeftSidebar
-        </div>
-    );
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map(link => {
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className="leftsidebar_link"
+            >
+              <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          )
+        })}
+      </div>
+    </section>
+  );
 }
