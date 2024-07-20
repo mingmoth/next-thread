2a8b35ad1ed4d9ea1bcfee97f2dc73825704be8f
Guanmin Liao <77824905+mingmoth@users.noreply.github.com>
2024-07-20 17:05:13 +0800

    [Feat] Add Root Page Layouts (#1)
    * feat: initialize TopBar & modify metadata
    
    * feat: add SignedIn with SignOutButton & OrganizationSwitcher in TopBar
    
    * feat: add constant index.ts for sidebarLinks & use it in LeftSidebar
    
    * feat: add isActive para for active nav route
    
    * feat: add SignedIn & SignOutButton in LeftSidebar
    
    * feat: add BottomBar with same sidebarLinks in RWD setting
    
    * feat: add RightSidebar layout styling

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,7 +1,41 @@
+"use client"
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
+// constants
+import { sidebarLinks } from "@/constants";
+
 export default function LeftSidebar() {
-    return (
-        <div className="left-sidebar">
-            LeftSidebar
-        </div>
-    );
+  const pathname = usePathname();
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map(link => {
+          const isActive = (pathname.includes(link.route) && link.route.length > 1) || link.route === pathname;
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
+            >
+              <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          )
+        })}
+      </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton redirectUrl="/sign-in">
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image src={"/assets/logout.svg"} alt="logout" width={24} height={24} />
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
+    </section>
+  );
 }
