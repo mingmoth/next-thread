aac91f8d81e83005f44d191369cfeca1fa536a34
mingmoth <mingmoth@gmail.com>
2024-07-21 12:04:49 +0800

    chore: copy uploadthing react instruction

---
+++ lib/uploadthing.ts
@@ -0,0 +1,7 @@
+// From https://docs.uploadthing.com/api-reference/react
+import { generateComponents } from "@uploadthing/react";
+
+import type { OurFileRouter } from "~/app/api/uploadthing/core";
+
+export const { UploadButton, UploadDropzone } =
+  generateComponents<OurFileRouter>();
