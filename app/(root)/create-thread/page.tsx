761474720e3361a7aaadb56d5e4c4421e2b98e2f
mingmoth <mingmoth@gmail.com>
2024-07-24 23:19:14 +0800

    feat: add Create Thread Page with PostThread component

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,24 @@
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+// libs
+import { fetchUser } from "@/lib/actions/user.actions"
+// components
+import PostThread from "@/components/forms/PostThread"
+
+export default async function Page() {
+  const user = await currentUser()
+  if (!user) redirect('/sign-in')
+
+  // const userInfo = await fetchUser(user.id)
+
+  // if(!userInfo || !userInfo.onboarded) {
+  //     redirect('/onboarding')
+  // }
+  return (
+    <>
+      <h1 className="head-text">Create Thread</h1>
+      <PostThread userId={user.id} />
+    </>
+
+  )
+}
---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,7 @@
+export default function PostThread({ userId }: { userId: string }) {
+    return (
+        <>
+            <h1>Post Thread Form</h1>
+        </>
+    )
+}
