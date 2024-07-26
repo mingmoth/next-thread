3f18d998bd9f8ad3bdc6bc516f490cdd101bd646
mingmoth <mingmoth@gmail.com>
2024-07-26 14:07:19 +0800

    feat: add some similar feature import into PostThread.tsx from AccountProfile

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,7 +1,43 @@
-export default function PostThread({ userId }: { userId: string }) {
-    return (
-        <>
-            <h1>Post Thread Form</h1>
-        </>
-    )
+"use client"
+
+import * as z from "zod"
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+import { usePathname, useRouter } from "next/navigation"
+// components
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  // FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Textarea } from "../ui/textarea"
+// libs
+import { ThreadValidation } from '@/lib/validations/thread'
+// import { updateUser } from "@/lib/actions/user.actions"
+
+
+interface Props {
+  userId: string
+}
+
+export default function PostThread({ userId }: Props) {
+  const form = useForm({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: '',
+      accountId: userId,
+    }
+  })
+
+  return (
+    <>
+      <h1>Post Thread Form</h1>
+    </>
+  )
 }
