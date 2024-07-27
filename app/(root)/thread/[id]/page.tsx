16db69d88e11afe4e8f24eb4f201674f93d00185
Guanmin Liao <77824905+mingmoth@users.noreply.github.com>
2024-07-27 22:09:40 +0800

    [Feat] Add Thread Page with ThreadCard & Comment component (#7)
    * fix: modify Comment author type & ThreadProps comments type
    
    * feat: add addCommentToThread function
    
    * feat: add Comment.tsx with addCommentToThread functionality
    
    * feat: add thread page with ThreadCard & Comment component

---
+++ app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,90 @@
+// components
+import Comment from "@/components/forms/Comment";
+import ThreadCard from "@/components/cards/ThreadCard";
+// lib
+import { fetchThreadById } from "@/lib/actions/thread.actions";
+
+export default async function Thread({ params }: { params: { id: string } }) {
+  // const thread = await fetchThreadById(params.id)
+
+  // mock data
+  const thread = {
+    id: '1',
+    text: 'Next-Thread First Post in the Community',
+    parentId: 'parent1',
+    author: {
+      id: 'user1',
+      name: 'User 1',
+      image: 'https://i.pravatar.cc/300',
+    },
+    createdAt: new Date(),
+    children: [
+      {
+        id: '1-1',
+        text: 'Thread 1-1',
+        parentId: '1',
+        author: {
+          id: 'user-2',
+          name: 'User 2',
+          image: 'https://i.pravatar.cc/300',
+        },
+        createdAt: new Date(),
+        comments: [],
+        community: null,
+      },
+      {
+        id: '1-2',
+        text: 'Thread 1-2',
+        parentId: '1',
+        author: {
+          id: 'user-3',
+          name: 'User 3',
+          image: 'https://i.pravatar.cc/500',
+        },
+        createdAt: new Date(),
+        comments: [],
+        community: null,
+      },
+    ],
+    community: null,
+  }
+
+  return (
+    <section>
+      <ThreadCard
+        key={thread.id}
+        id={thread.id}
+        author={thread.author}
+        comments={thread.children}
+        community={thread.community}
+        content={thread.text}
+        createdAt={thread.createdAt}
+        currentUserId="user1"
+        isComment={thread.children?.length > 0}
+        parentId={thread.parentId}
+      />
+      <div className="mt-7">
+        <Comment
+          threadId={thread.id}
+          userId={thread.author.id}
+          userImg={thread.author.image}
+        />
+      </div>
+      <div className="mt-10">
+        {thread.children?.map((child) => (
+          <ThreadCard
+            key={child.id}
+            id={child.id}
+            author={child.author}
+            content={child.text}
+            createdAt={child.createdAt}
+            currentUserId="user1"
+            parentId={child.parentId}
+            community={child.community || null}
+            comments={child.comments || []}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
