47dc89f422a766de8268d9ae9e25de3315e6161d
mingmoth <mingmoth@gmail.com>
2024-07-28 12:30:46 +0800

    feat: add ProfileHeader component in Profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -4,6 +4,8 @@ import { redirect } from "next/navigation"
 import { fetchUser } from "@/lib/actions/user.actions"
 // constants
 import { profileTabs } from "@/constants"
+// components
+import ProfileHeader from "@/components/shared/ProfileHeader"
 
 export default async function Profile({ params }: { params: { id: string } }) {
   // const userInfo = await fetchUser(params.id)
@@ -15,9 +17,33 @@ export default async function Profile({ params }: { params: { id: string } }) {
   //     </section>
   //   )
   // }
+
+  // mock data
+  const userInfo = {
+    id: '1',
+    name: 'User 1',
+    username: 'user1',
+    bio: 'User 1 Bio',
+    image: 'https://i.pravatar.cc/300',
+  }
+  const user = {
+    id: '2',
+    name: 'User 2',
+    username: 'user2',
+    bio: 'User 2 Bio',
+    image: 'https://i.pravatar.cc/300',
+  }
+
   return (
     <section>
-      <h1 className="head-text">Profile</h1>
+      <ProfileHeader
+        accountId={userInfo.id}
+        authUserId={user.id}
+        name={userInfo.name}
+        username={userInfo.username}
+        image={userInfo.image}
+        bio={userInfo.bio}
+      />
     </section>
   )
 }
