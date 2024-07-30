0ea6faeed54ade38503a40bf905b9b1df68863dc
mingmoth <mingmoth@gmail.com>
2024-07-30 22:05:43 +0800

    feat: add isComment style in ThreadCard

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -60,7 +60,7 @@ export default function ThreadCard({
             </Link>
             <p className="mt-2 text-small-regular text-light-2">{content}</p>
 
-            <div className="mt-5 flex flex-col gap-3">
+            <div className={`mt-5 ${isComment && 'mb-10'} flex flex-col gap-3`}>
               <div className="flex gap-3.5">
                 <Image src="/assets/heart-gray.svg" alt="Like" width={24} height={24} className="cursor-pointer object-contain"/>
                 <Link href={`/thread/${id}`}>
