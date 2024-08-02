c2fb2ef42a6fe6a6af78debca5440c232910929e
Guanmin Liao <77824905+mingmoth@users.noreply.github.com>
2024-08-02 17:53:10 +0800

    [Feat] Search Page (#9)
    * feat: add SearchBar component in Search Page
    
    * fix: lint fix
    
    * chore: add shadcn pagination
    
    * feat: add UserCard & use it in search page
    
    * feat: add fetchUsers function in user.action
    
    * feat: add Next Link functionality in shadcn Pagination
    
    * feat: add Pagination in search page

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,110 @@
+"use client"
+
+import { usePathname, useRouter } from "next/navigation";
+import UserCard from "@/components/cards/UserCard";
+import SearchBar from "@/components/shared/SearchBar";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
+interface User {
+  id: string,
+  name: string,
+  username: string,
+  imgUrl?: string,
+  personType: string,
+  onClick?: () => void
+}
+
+export default function Search({
+  searchParams
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const router = useRouter()
+  const pathname = usePathname()
+  const routeType = pathname.split('/')[1]
+
+  const result = {
+    users: [
+      {
+        id: '1',
+        name: 'John Doe',
+        username: 'johndoe',
+        imgUrl: 'https://i.pravatar.cc/300',
+        personType: 'User'
+      },
+      {
+        id: '2',
+        name: 'Mary Jane',
+        username: 'maryjane',
+        imgUrl: '',
+        personType: 'Community'
+      }
+    ],
+    totalPage: 5,
+    isNext: true
+  }
+
+  const onClickUser = (user: User) => {
+    const isCommunity = user.personType === 'Community'
+    router.push(`/${isCommunity ? 'community' : 'profile'}/${user.id}`)
+  }
+
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+      <SearchBar routeType={routeType} />
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.map((user) => (
+          <UserCard
+            key={user.id}
+            id={user.id}
+            name={user.name}
+            username={user.username}
+            imgUrl={user.imgUrl}
+            personType={user.personType}
+            onClick={() => onClickUser(user)}
+          />
+        ))}
+      </div>
+
+      {(result.isNext && result.totalPage > 1) && <Pagination className="text-light-2">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              href={`/search?page=${Number(searchParams.page) - 1 > 0 ? Number(searchParams.page) - 1 : 1}`}
+              aria-disabled={!!searchParams.page && Number(searchParams?.page) > 1}
+            />
+          </PaginationItem>
+          {result.totalPage && result.totalPage <= 5 && [...Array(result.totalPage)].map((_, index) => (
+            <PaginationItem key={index}>
+              <PaginationLink
+                href={`/search?page=${index + 1}`}
+                isActive={Number(searchParams.page) === index + 1}
+              >
+                  {index + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          {/* <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem> */}
+          <PaginationItem>
+            <PaginationNext
+              href={`/search?page=${Number(searchParams.page) + 1 >= Number(result.totalPage) ? result.totalPage : Number(searchParams.page) + 1}`}
+              aria-disabled={!!searchParams.page && Number(searchParams?.page) >= result.totalPage}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>}
+    </section>
+  )
+}
---
+++ components/shared/SearchBar.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { useState } from "react";
+import { Input } from "../ui/input";
+
+export default function SearchBar({ routeType }: { routeType: string }) {
+  const getSearchType = () => {
+    switch (routeType) {
+      case 'search':
+        return 'Users'
+      default:
+        return 'Communities'
+    }
+  }
+
+  const [search, setSearch] = useState('')
+
+  return (
+    <div className="searchbar">
+      <Image
+        src='/assets/search-gray.svg'
+        alt='search'
+        width={24}
+        height={24}
+        className='object-contain'
+      />
+      <Input
+        id='text'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder={`Search ${ getSearchType() }`}
+        className='no-focus searchbar_input'
+      />
+    </div>
+  )
+}
