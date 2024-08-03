c5d59f8ea23c80cbb61be682afbf051c0865215d
mingmoth <mingmoth@gmail.com>
2024-08-03 14:41:29 +0800

    feat: add getUserActivities action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { FilterQuery, SortOrder } from "mongoose"
 import { revalidatePath } from "next/cache"
+import Thread from "../models/thread.model";
 import User from "../models/user.model"
 import { connectToDB } from "../mongoose"
 import path from "path"
@@ -153,6 +154,35 @@ export async function fetchUsers({
     return { users, totalPage, isNext };
   } catch (error) {
     console.error("Error fetching users:", error);
+    throw error;
+  }
+}
+
+export async function getUserActivities(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads created by the user
+    const userThreads = await Thread.find({ author: userId });
+
+    // Collect all the child thread ids (replies) from the 'children' field of each user thread
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    // Find and return the child threads (replies) excluding the ones created by the same user
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId }, // Exclude threads authored by the same user
+    }).populate({
+      path: "author",
+      model: User,
+      select: "_id name image",
+    });
+
+    return replies;
+  } catch (error) {
+    console.error("Error fetching replies: ", error);
     throw error;
   }
 }
